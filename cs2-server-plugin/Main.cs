027e0032232868bc3e40f80443c7dfa2fa0cbc9e
JacobNorlin <jacob.norlinandersson@gmail.com>
2024-06-04 21:18:17 +0200

    Add CS2 server plugin

---
+++ cs2-server-plugin/Main.cs
@@ -0,0 +1,16 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+
+namespace cs2_server_plugin;
+
+public class Main : BasePlugin
+{
+
+  public override string ModuleName => "Giga inhouse";
+  public override string ModuleVersion => "0.0.1";
+  public override void Load(bool hotReload)
+  {
+    Console.WriteLine("Loaded giga-inhouse plugin");
+  }
+
+}
